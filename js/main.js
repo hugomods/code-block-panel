54849427fd9edfc6ef6314b528d751b05fffcbe4
razonyang <razonyang@users.noreply.github.com>
2023-01-12 14:59:10 +0000

    deploy: 9b5ea4cd4cf94c46c9f137de2e522e3f1df65409

---
+++ js/main.js
@@ -0,0 +1,187 @@
+(() => {
+})();
+
+;
+(() => {
+  // ns-params:@params
+  var params_default = { defaultLang: "en", i18n: { en: { copied: { other: "Copied!" }, copy_failed: { other: "Copy failed!" } } }, icons: { copy: '\n  \n  \n  \n    \n  \n    <svg class="bi bi-clipboardbi bi-clipboard hi-svg-inline" fill="currentColor" height="1.25rem" viewBox="0 0 16 16" width="1.25rem" xmlns="http://www.w3.org/2000/svg">\n  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>\n  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>\n</svg>', expand: '\n  \n  \n  \n    \n  \n    <svg class="bi bi-chevron-expandbi bi-chevron-expand hi-svg-inline" fill="currentColor" height="1.25rem" viewBox="0 0 16 16" width="1.25rem" xmlns="http://www.w3.org/2000/svg">\n  <path fill-rule="evenodd" d="M3.646 9.146a.5.5 0 0 1 .708 0L8 12.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-2.292a.5.5 0 0 0 .708 0L8 3.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708z"/>\n</svg>', ln: '\n  \n  \n  \n    \n  \n    <svg class="bi bi-list-olbi bi-list-ol hi-svg-inline" fill="currentColor" height="1.25rem" viewBox="0 0 16 16" width="1.25rem" xmlns="http://www.w3.org/2000/svg">\n  <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"/>\n  <path d="M1.713 11.865v-.474H2c.217 0 .363-.137.363-.317 0-.185-.158-.31-.361-.31-.223 0-.367.152-.373.31h-.59c.016-.467.373-.787.986-.787.588-.002.954.291.957.703a.595.595 0 0 1-.492.594v.033a.615.615 0 0 1 .569.631c.003.533-.502.8-1.051.8-.656 0-1-.37-1.008-.794h.582c.008.178.186.306.422.309.254 0 .424-.145.422-.35-.002-.195-.155-.348-.414-.348h-.3zm-.004-4.699h-.604v-.035c0-.408.295-.844.958-.844.583 0 .96.326.96.756 0 .389-.257.617-.476.848l-.537.572v.03h1.054V9H1.143v-.395l.957-.99c.138-.142.293-.304.293-.508 0-.18-.147-.32-.342-.32a.33.33 0 0 0-.342.338v.041zM2.564 5h-.635V2.924h-.031l-.598.42v-.567l.629-.443h.635V5z"/>\n</svg>', wrap: '\n  \n  \n  \n    \n  \n    <svg class="bi bi-text-wrapbi bi-text-wrap hi-svg-inline" fill="currentColor" height="1.25rem" viewBox="0 0 16 16" width="1.25rem" xmlns="http://www.w3.org/2000/svg">\n  <path fill-rule="evenodd" d="M2 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5Zm0 4a.5.5 0 0 1 .5-.5h9a2.5 2.5 0 0 1 0 5h-1.293l.647.646a.5.5 0 0 1-.708.708l-1.5-1.5a.5.5 0 0 1 0-.708l1.5-1.5a.5.5 0 0 1 .708.708l-.647.646H11.5a1.5 1.5 0 0 0 0-3h-9a.5.5 0 0 1-.5-.5Zm0 4a.5.5 0 0 1 .5-.5H7a.5.5 0 0 1 0 1H2.5a.5.5 0 0 1-.5-.5Z"/>\n</svg>' }, max_lines: 10 };
+
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/razonyang/hugo-mod-snackbar@v0.1.0/assets/snackbar/js/index.ts
+  var Snackbar = class {
+    container;
+    getContainer() {
+      if (!this.container) {
+        this.container = document.createElement("div");
+        this.container.className = "snackbars";
+        document.body.appendChild(this.container);
+      }
+      return this.container;
+    }
+    add(text, duration = 2e3) {
+      const msg = document.createElement("div");
+      msg.className = "snackbar";
+      msg.innerText = text;
+      this.getContainer().appendChild(msg);
+      setTimeout(() => {
+        msg.remove();
+      }, duration);
+    }
+  };
+  var snackbar = new Snackbar();
+  var js_default = snackbar;
+
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/razonyang/hugo-mod-i18n-js@v0.2.1/assets/js/i18n/translator.ts
+  var Translator = class {
+    constructor(translations, fallback) {
+      this.translations = translations;
+      this.fallback = fallback;
+    }
+    lang = "";
+    getLang() {
+      if (this.lang === "") {
+        this.lang = document.documentElement.getAttribute("lang") ?? this.fallback;
+      }
+      return this.lang;
+    }
+    getTranslations() {
+      const lang = this.getLang();
+      return lang in this.translations ? this.translations[this.getLang()] : this.translations[this.fallback];
+    }
+    getFallbackTranslation(key) {
+      return this.translations[this.fallback][key] ?? "";
+    }
+    translate(key, ctx) {
+      const translations = this.getTranslations();
+      const translation = translations[key] ?? this.getFallbackTranslation(key);
+      if (!ctx) {
+        return translation.other;
+      }
+      let format = ctx.count === 1 ? translation.one : translation.other;
+      for (let name in ctx) {
+        format = format.replace(`{${name}}`, ctx[name]);
+      }
+      return format;
+    }
+  };
+
+  // ns-hugo:/home/runner/work/hugo-mod-code-block-panel/hugo-mod-code-block-panel/assets/code-block-panel/js/i18n.ts
+  var i18n = new Translator(params_default.i18n, params_default.defaultLang);
+  var i18n_default = i18n;
+
+  // ns-hugo:/home/runner/work/hugo-mod-code-block-panel/hugo-mod-code-block-panel/assets/code-block-panel/js/panel.ts
+  var Panel = class {
+    constructor(code) {
+      this.code = code;
+    }
+    pre;
+    wrapper;
+    ele;
+    init() {
+      this.pre = this.code.parentElement;
+      this.ele = document.createElement("div");
+      this.ele.className = "code-block-panel";
+      this.wrapper = document.createElement("div");
+      this.wrapper.className = "code-block-panel-wrapper";
+      this.wrapper.appendChild(this.ele);
+      this.maxLines();
+      this.language();
+      this.lineNoButton();
+      this.wrapButton();
+      this.expandButton();
+      this.copyButton();
+      this.pre.appendChild(this.wrapper);
+    }
+    lines() {
+      return Array.from(this.code.querySelectorAll(":scope > span"));
+    }
+    maxHeight;
+    maxLines() {
+      const lines = this.lines();
+      if (params_default.max_lines > 0 && lines.length > params_default.max_lines) {
+        this.pre.style.maxHeight = this.maxHeight = lines[params_default.max_lines - 1].offsetTop + "px";
+      }
+    }
+    language() {
+      const lang = this.code.getAttribute("data-lang");
+      if (!lang || lang === "fallback") {
+        return;
+      }
+      const e = document.createElement("span");
+      e.className = "code-block-lang";
+      e.innerText = lang;
+      this.pre.appendChild(e);
+    }
+    button(name, callback) {
+      const btn = document.createElement("button");
+      btn.className = "code-block-action code-block-action-" + name;
+      btn.innerHTML = params_default.icons[name];
+      btn.addEventListener("click", () => {
+        callback();
+      });
+      return btn;
+    }
+    copyButton() {
+      const btn = this.button("copy", () => {
+        this.copy();
+      });
+      this.ele.appendChild(btn);
+    }
+    copy() {
+      const clone = this.code.cloneNode(true);
+      clone.querySelectorAll(".ln, :scope > span > span:first-child").forEach((ln) => {
+        ln.remove();
+      });
+      navigator.clipboard.writeText(clone.innerText).then(() => {
+        js_default.add(i18n_default.translate("copied"));
+      }).catch((err) => {
+        js_default.add(i18n_default.translate("copy_failed"));
+        console.error(err);
+      });
+    }
+    wrapButton() {
+      const btn = this.button("wrap", () => {
+        this.toggleClass("code-wrap");
+      });
+      this.ele.appendChild(btn);
+    }
+    toggleClass(className) {
+      if (this.code.classList.contains(className)) {
+        this.code.classList.remove(className);
+        return;
+      }
+      this.code.classList.add(className);
+    }
+    lineNoButton() {
+      const btn = this.button("ln", () => {
+        this.toggleClass("code-ln");
+      });
+      this.ele.appendChild(btn);
+    }
+    expandButton() {
+      const btn = this.button("expand", () => {
+        this.expand();
+      });
+      this.ele.appendChild(btn);
+    }
+    expand() {
+      if (!this.pre.style.maxHeight && !this.maxHeight) {
+        return;
+      }
+      if (this.pre.style.maxHeight) {
+        this.pre.style.maxHeight = "";
+        return;
+      }
+      this.pre.style.maxHeight = this.maxHeight;
+    }
+  };
+
+  // <stdin>
+  (() => {
+    "use strict";
+    document.addEventListener("DOMContentLoaded", () => {
+      document.querySelectorAll("pre code").forEach((code) => {
+        new Panel(code).init();
+      });
+    });
+  })();
+})();
